e873360dfdcec98ad462bbf0f26afcb69dc1fcff
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-02-02 22:20:07 +0100

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,12 +1,17 @@
 #include "lib/io_interrup.h"
+#include "lib/utils/math.h"
 #include "lib/io.h"
 #include "lib/alloc/allocatore.h"
 
 void main (){
 	//startVGA();
-	TAD(1);
-	print("CIAO\tciao\b\n\tciao", VGA_TEXT_BIANCO_NERO);
-	
+//	alloc(2*1024);
+//	alloc(3*2048);
+//	alloc(1);
+//	alloc(3);
+//	print("CIAO\tciao\b\n\tciao", VGA_TEXT_BIANCO_NERO);
+	printint(12, VGA_TEXT_BIANCO_NERO);
+
 	char buffer[128];
 	input(buffer, 0x1c, sizeof(buffer), VGA_TEXT_BIANCO_NERO);
 	while(1){	__asm__("hlt");};
