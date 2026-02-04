bcaef9c8f214b03f2acb076bf764697b9a31e8fc
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-02-04 21:48:28 +0100

    Add files via upload

---
+++ lib/cli/shell.h
@@ -0,0 +1,37 @@
+#include "struct.h"
+
+Comando comandi[] = {{"print", printS}};
+
+bool CLIparser(char *buffer){
+	unsigned int contatore_buffer = 0;	
+	while (buffer[contatore_buffer] != '\0'){	contatore_buffer++;}
+	for (unsigned int contatore_comandi = 0; contatore_comandi < (sizeof(comandi) / sizeof(comandi[contatore_comandi])); contatore_comandi++){
+			unsigned int caratteri_giusti = 0;
+			while(comandi[contatore_comandi].nome_comando[caratteri_giusti] == buffer[caratteri_giusti]){
+				if (caratteri_giusti == contatore_buffer){
+					comandi[contatore_comandi].funzione();
+					return true;
+				}
+				caratteri_giusti++;
+			}	
+	}
+	print("\nCOMANDO INVALIDO", VGA_TEXT_ROSSO_NERO);
+	return false;
+}
+
+bool CLIparserGenerico (char *buffer, Comando *comandi, int sizeComandi){
+	unsigned int contatore_buffer = 0;	
+	while (buffer[contatore_buffer] != '\0'){	contatore_buffer++;}
+	for (unsigned int contatore_comandi = 0; contatore_comandi < sizeComandi; contatore_comandi++){
+			unsigned int caratteri_giusti = 0;
+			while(comandi[contatore_comandi].nome_comando[caratteri_giusti] == buffer[caratteri_giusti]){
+				if (caratteri_giusti == contatore_buffer){
+					comandi[contatore_comandi].funzione();
+					return true;
+				}
+				caratteri_giusti++;
+			}	
+	}
+	print("\nCOMANDO INVALIDO", VGA_TEXT_ROSSO_NERO);
+	return false;
+}
---
+++ lib/cli/struct.h
@@ -0,0 +1,4 @@
+typedef struct{
+	char nome_comando[16];
+	void (*funzione)();
+}Comando;
