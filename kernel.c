980a28e5e8c98cf20da03321da5b9bb1a484670c
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-02-04 21:46:30 +0100

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -3,14 +3,20 @@
 #include "lib/io.h"
 #include "lib/alloc/allocatore.h"
 
+#include "lib/cli/shell.h"
+
 void main (){
 	//startVGA();
-	infoAlloc();
+//	infoAlloc();
 
-	int *a = (int *)alloc(3);
-	free(a);
+//	int *a = (int *)alloc(3);
+//	free(a);
 
-	char buffer[128];
-	input(buffer, 0x1c, sizeof(buffer), VGA_TEXT_BIANCO_NERO);
+	while (1){
+		char buffer[128];
+		printchar('#', VGA_TEXT_BIANCO_NERO);
+		input(buffer, 0x39, sizeof(buffer), VGA_TEXT_BIANCO_NERO);
+		CLIparser(buffer);
+	}
 	while(1){	__asm__("hlt");};
 }
