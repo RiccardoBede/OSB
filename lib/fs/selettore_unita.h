7bcbbf98f200aae1e621a1b8e04d853adc8a1ab7
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-02-15 21:22:48 +0100

    Add files via upload

--- lib/fs/selettore_unita.h
+++ lib/fs/selettore_unita.h
@@ -11,6 +11,7 @@
 #define ATA_FLUSH_CACHE 0xe7
 
 //DEFINE COMANDI IDE_ATAPI (CD, DVD)
+#define ATAPI_IDENTIFICA 0x51
 #define ATAPI_READY 0x00
 
 #define ATAPI_MONTA_SMONTA 0xa6
@@ -26,17 +27,32 @@ typedef enum {
 	DISCO_ATAPI = 0x02
 }DISCO_MONTATO; //per poi vedere quali set di funzioni unsare per scrivere sui dischi
 
-//porta default del controller
-unsigned short porta_controller_default 0x1f7
+struct ATA_IDENTIFICA {
+	char numeroSerie[10];
+	char versioneFirmware[4];
+	char modello[20];
+	//settori totali LBA32 FARE ALLOC
+	bool falgLBA48;
+	//settori totali LBA48 FARE ALLOC
+};
 
-cambia_unita (){
+struct ATAPI_IDENTIFICA {};
+
+//porta default del controller
+unsigned short porta_controller_default = 0x1f7;
+
+DISCO_MONTATO cambia_unita (){
 	unsigned int tentativi_lettura = 0;
 	while (tentativi_lettura < 10){
 		unsigned int attesa_busy = 0;
 		while (attesa_busy < 256){
 			if (!(inb(porta_controller_default) & ATA_BUSY)){
+				//ATA_IDE
 				outb(porta_controller_default, ATA_INDENTIFICA);
-				if (inb(porta_controller_default) & ATA_RICHIESTA_COMANDO_OK){	return;}
+				if (inb(porta_controller_default) & ATA_RICHIESTA_COMANDO_OK){	return 0x01;}
+				//ATAPI
+				outb(porta_controller_default, ATAPI_IDENTIFICA);
+				if (inb(porta_controller_default) & ATAPI_READY){	return 0x02;}
 			}
 			attesa_busy++;
 		}
@@ -48,10 +64,25 @@ cambia_unita (){
 		char porta[8];
 		input(porta, 0x1c, sizeof(porta), VGA_TEXT_BIANCO_NERO);
 		porta_controller_default = stringa_to_unsigned_short(porta);
+		printchar('\n', VGA_TEXT_BIANCO_NERO);
 		cambia_unita();
 	}else{
-	
+		return 0x00;
 	}
 }
 
-void identifica_unita (){}
+void identifica_unita (){
+	DISCO_MONTATO tipo_unita = cambia_unita();
+	print("UNITA' ARCHIVIAZIONE ", VGA_TEXT_BIANCO_NERO);
+	switch (tipo_unita){
+		case NESSUN_DISCO:
+			print("<NULL>\n", VGA_TEXT_ROSSO_NERO);
+			break;
+		case DISCO_ATA:
+			print("<ATA/IDE>\n", VGA_TEXT_VERDE_NERO);
+			break;
+		case DISCO_ATAPI:
+			print("<ATAPI>\n", VGA_TEXT_VERDE_NERO);
+			break;
+	}
+}
