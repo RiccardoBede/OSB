8d67c13fcba01b885e73f901f42c86b539ff51c2
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-02-15 19:08:58 +0100

    Add files via upload

--- lib/utils/conversioni.h
+++ lib/utils/conversioni.h
@@ -11,6 +11,19 @@ unsigned long int stringa_to_int (char *buffer){
 	return valore_finale;
 }
 
+unsigned short stringa_to_unsigned_short (char *buffer){
+	unsigned int contatore_cifre = 0;
+	unsigned long int valore_finale = 0;
+	while (buffer[contatore_cifre] != '\0'){	contatore_cifre++;}
+	for (unsigned int contatore = 0; contatore < contatore_cifre; contatore++){
+		valore_finale *= 10;
+		if (buffer[contatore] >= '0' && buffer[contatore] <= '9'){
+			valore_finale += buffer[contatore] - '0';
+		}
+	}
+	return valore_finale;
+}
+
 unsigned long int stringa_to_hex (char *buffer){
 	unsigned int contatore_cifre = 0;
 	unsigned long int valore_finale = 0;
