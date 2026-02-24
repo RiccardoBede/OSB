2872ed567c04269290afad948fad5029ebeeb1eb
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-02-24 09:47:00 +0100

    Add files via upload

--- lib/utils/conversioni.h
+++ lib/utils/conversioni.h
@@ -39,3 +39,23 @@ unsigned long int stringa_to_hex (char *buffer){
 	}
 	return valore_finale;
 }
+
+char* num_to_char_bin (unsigned long int numero_decimale){
+	char buffer_char[32];
+	unsigned long int numero_cifre_decimale = numero_decimale;
+	unsigned int numero_cifre_bin = 0;
+	while (numero_cifre_decimale > 0){
+		numero_cifre_decimale /= 2;
+		numero_cifre_bin++;
+	}
+	unsigned int cifra = 0;
+	while (cifra < (sizeof(numero_decimale) - numero_cifre_bin)){
+		buffer_char[cifra] = 0x30;
+		cifra++;
+	}
+	while (cifra < numero_cifre_bin){
+		buffer_char[cifra] = ((numero_decimale << cifra) & 1) ? 0x31 : 0x30;
+		cifra++;
+	}
+	return buffer_char;
+}
