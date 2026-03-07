6362ea432fb183279aade994edcd5122a233349b
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-03-07 12:52:26 +0100

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -9,6 +9,7 @@
 #include "lib/cli/shell.h"
 
 #include "lib/fs/selettore_unita.h"
+#include "lib/fs/crud_settore.h"
 
 void main (){
 	clear();
@@ -20,6 +21,9 @@ void main (){
 //	free(a);
 
 	tipo_unita_montata();
+/*	char buffer[512];
+	input(buffer, 0x1c, sizeof(buffer), VGA_TEXT_BIANCO_NERO);
+	scrivi_settore(0x01, 1, buffer, 0x00);*/
 
 	while (1){
 		char buffer[128];
