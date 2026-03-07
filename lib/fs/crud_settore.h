b5187549d114975370ab8f94f3b7868ec94a3629
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-03-07 19:51:41 +0100

    Add files via upload

--- lib/fs/crud_settore.h
+++ lib/fs/crud_settore.h
@@ -1,5 +1,3 @@
-//#include "selettore_unita.h"
-
 #define SIZE_SETTORE 512
 
 extern bool lba48_attivo;
@@ -11,6 +9,21 @@ typedef struct{
 	char primo_byte;
 	char secondo_byte;
 }ByteSettore __attribute__((packed));
+
+void outw_d (unsigned short porta, ByteSettore buffer){
+	__asm__ ("outw %0, %1" :: "a" (buffer), "dN" (porta));
+}
+
+ByteSettore inw_d (unsigned short porta){
+	ByteSettore buffer;
+	unsigned short valore;
+	__asm__ ("inw %%dx, %%ax" : "=a" (valore) : "dN" (porta));
+
+	buffer.primo_byte = valore & 0xFF;
+    	buffer.secondo_byte = (valore >> 8) & 0xFF;
+
+	return buffer;
+}
 
 typedef enum{
 	SETTORE_NORMALE = 0x00,
@@ -50,20 +63,65 @@ bool scrivi_settore (DISCO_MONTATO tipo_disco, unsigned long int numero_settore,
 		while(!(inb(porta_controller_default)) & ATA_BUSY);
 		
 		while (conta_carattere_settore < SIZE_SETTORE){
-			if (conta_carattere_settore < (caratteri_buffer)){
+			if (conta_carattere_settore <  (caratteri_buffer - 1)){
 				carattere_buffer_per_settore.primo_byte = buffer[conta_carattere_settore];
-			//	carattere_buffer_per_settore.secondo_byte = buffer[conta_carattere_settore + 1];
+				carattere_buffer_per_settore.secondo_byte = buffer[conta_carattere_settore+=1];
 
-				outw((porta_controller_default - 0x07), (char)carattere_buffer_per_settore.primo_byte);
+				outw_d((porta_controller_default - 0x07), carattere_buffer_per_settore);
 			}else{
 				carattere_buffer_per_settore.primo_byte = 0x00;
 				carattere_buffer_per_settore.secondo_byte = 0x00;
 
-				outw((porta_controller_default - 0x07), (char)carattere_buffer_per_settore.primo_byte);
+				outw_d((porta_controller_default - 0x07), carattere_buffer_per_settore);	
 			}
-			conta_carattere_settore += 1;//2;
+			conta_carattere_settore++;
 		}
 		outb(porta_controller_default, ATA_FLUSH_CACHE);
+	}else{
+	
+	}
+}
+
+bool leggi_settore (DISCO_MONTATO tipo_disco, unsigned long int numero_settore, unsigned char *buffer, int sizeof_buffer){
+	if (tipo_disco > 0x00){
+		tipo_disco -= 0x01;
+	}else{
+		return false;
+	}
+	
+	ByteSettore carattere_buffer_per_settore;
+	unsigned short int conta_carattere_settore = 0;
+	
+	if (!lba48_attivo){
+		while(inb(porta_controller_default) & ATA_BUSY);
+
+		while(!(inb(porta_controller_default) & ATA_DEVICE_READY));
+
+		outb((porta_controller_default - 0x05), numero_settore);
+
+		outb((porta_controller_default - 0x04), (numero_settore << 0) & 0xff);
+		outb((porta_controller_default - 0x03), (numero_settore << 8) & 0xff);
+		outb((porta_controller_default - 0x02), (numero_settore << 16) & 0xff);
+
+		outb((porta_controller_default - 0x01), ATA_ACCESSO_DISCO | ((tipo_disco & 1) << 4) | ((numero_settore >> 24) & 0x0f));
+	
+		outb(porta_controller_default, ATA_LEGGI_SETTORE);
+
+		while(!(inb(porta_controller_default)) & ATA_BUSY);
+		
+		while (conta_carattere_settore < SIZE_SETTORE && conta_carattere_settore < sizeof_buffer){
+			if (conta_carattere_settore < (sizeof_buffer - 1)){
+				ByteSettore carattere_settore = inw_d((porta_controller_default - 0x07));
+
+				buffer[conta_carattere_settore] = (char)carattere_settore.primo_byte;
+				buffer[conta_carattere_settore+=1] = (char)carattere_settore.secondo_byte;
+			}else{
+				buffer[conta_carattere_settore] = 0x00;
+			}
+			conta_carattere_settore++;
+		}
+		outb(porta_controller_default, ATA_FLUSH_CACHE);
+		buffer[sizeof_buffer] = '\0';
 	}else{
 	
 	}
