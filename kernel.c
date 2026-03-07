4abd43ce62b6926b6831d4f8846570648e5e38e7
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-03-07 19:52:14 +0100

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -23,8 +23,14 @@ void main (){
 	tipo_unita_montata();
 /*	char buffer[512];
 	input(buffer, 0x1c, sizeof(buffer), VGA_TEXT_BIANCO_NERO);
-	scrivi_settore(0x01, 1, buffer, 0x00);*/
+	scrivi_settore(0x01, 1, buffer, 0x00);
 
+	printchar('\n' ,VGA_TEXT_BIANCO_NERO);
+
+	char settore[512];
+	leggi_settore(0x01, 1, buffer, sizeof(buffer));
+	print(buffer, VGA_TEXT_BIANCO_NERO);
+*/
 	while (1){
 		char buffer[128];
 		printchar('#', VGA_TEXT_BIANCO_NERO);
