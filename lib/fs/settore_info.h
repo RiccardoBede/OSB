91c54c24153d41060210f2f047e6267e324fe817
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-03-25 22:06:07 +0100

    Add files via upload

--- lib/fs/settore_info.h
+++ lib/fs/settore_info.h
@@ -7,43 +7,37 @@ extern struct settori_lba48;
 extern unsigned short porta_controller_default;
 
 static unsigned char settore_info[1024];
-unsigned char array_flag[][10] = {"filesystem"/*, "allocator"*/}; 
+unsigned char array_flag[][16] = {"filesystem", "allocator"}; 
 
 void applica_settore_info (unsigned char *buffer_settore, unsigned int sizeof_buffer_settore){
 	unsigned short int numero_flag = 0;
 	unsigned short int contatore_carattere_flag = 0;
 	unsigned short int sizeof_flag_corrente = 0;
 
-	for (unsigned short int contatore_carattere_buffer = 0; contatore_carattere_buffer < sizeof_buffer_settore; contatore_carattere_buffer++){
-		while (array_flag[numero_flag][sizeof_flag_corrente] != '\0'){	sizeof_flag_corrente++;}
-
-		if (buffer_settore[contatore_carattere_buffer] == array_flag[numero_flag][contatore_carattere_flag] && (numero_flag < (sizeof(array_flag) / sizeof(array_flag[numero_flag])))){
-			if (contatore_carattere_flag == sizeof_flag_corrente - 1){
-				print("\t*", VGA_TEXT_GIALLO_NERO);
-				print(array_flag[numero_flag], VGA_TEXT_GIALLO_NERO);
-				printchar(' ', VGA_TEXT_GIALLO_NERO);
-				
-				contatore_carattere_buffer++;
-				while (buffer_settore[contatore_carattere_buffer] != ';'){
-					printchar(buffer_settore[contatore_carattere_buffer], VGA_TEXT_GIALLO_NERO);
-					contatore_carattere_buffer++;
-				}
-
-				printchar('\n', VGA_TEXT_GIALLO_NERO);
-				numero_flag++;
-				contatore_carattere_flag = 0;
-				sizeof_flag_corrente = 0;
-			}
-			contatore_carattere_flag++;
-		}else{
-			/*if (numero_flag < (sizeof(array_flag) / sizeof(array_flag[0]))){
-				numero_flag++;
-			}else{
-				numero_flag = 0;
-			}*/
-			contatore_carattere_flag = 0;
-		}
+	for (numero_flag; numero_flag < (sizeof(array_flag) / sizeof(array_flag[0])); numero_flag++){
+		contatore_carattere_flag = 0;
 		sizeof_flag_corrente = 0;
+		while (array_flag[numero_flag][sizeof_flag_corrente] != '\0'){	sizeof_flag_corrente++;}	
+
+		for (unsigned short int contatore_carattere_buffer = 0; contatore_carattere_buffer < sizeof_buffer_settore; contatore_carattere_buffer++){
+			if (buffer_settore[contatore_carattere_buffer] == array_flag[numero_flag][contatore_carattere_flag]){
+				if (contatore_carattere_flag == sizeof_flag_corrente - 1){
+					print("\t*", VGA_TEXT_GIALLO_NERO);
+					print(array_flag[numero_flag], VGA_TEXT_GIALLO_NERO);
+					printchar(' ', VGA_TEXT_GIALLO_NERO);
+				
+					contatore_carattere_buffer++;
+					while (buffer_settore[contatore_carattere_buffer] != ';'){
+						printchar(buffer_settore[contatore_carattere_buffer], VGA_TEXT_GIALLO_NERO);
+						contatore_carattere_buffer++;
+					}
+
+					printchar('\n', VGA_TEXT_GIALLO_NERO);
+					contatore_carattere_flag = 0;
+				}
+				contatore_carattere_flag++;
+			}else{	contatore_carattere_flag = 0;}	
+		}
 	}
 }
 
