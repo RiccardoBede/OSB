e43e8f09b2abdf76f9dcc24b0a21b9f89a0fb57f
RiccardoBede <99608854+RiccardoBede@users.noreply.github.com>
2026-05-15 18:08:28 +0200

    Add files via upload

---
+++ lib/cli/comandi_integrati/helpCli.h
@@ -0,0 +1,8 @@
+void helpCli (){
+	print("\nhelp : lista dei comandi\n", VGA_TEXT_BIANCO_NERO);
+	print("inb  : (porta) leggi il valore di una porta\n", VGA_TEXT_BIANCO_NERO);
+	print("outb : (porta, comando) inserisci comando in una porta\n", VGA_TEXT_BIANCO_NERO);
+	print("alloc: (size) alloca dinamicamente (size)byte\n", VGA_TEXT_BIANCO_NERO);
+	print("free : (puntatore) dealloca la zona di ram allocata\n", VGA_TEXT_BIANCO_NERO);
+	print("video: [13h] imposta la modalita video\n", VGA_TEXT_BIANCO_NERO);
+}
